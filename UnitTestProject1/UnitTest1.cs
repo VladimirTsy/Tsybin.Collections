a079db47f0939b2c7d3f7b0e1b6d59b77e49d503
Vladimir Tsybin <vladimirtsybin@yandex.ru>
2019-11-30 21:25:34 +0700

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using myCollection;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestExpected10And20()
+        {
+            var extDict = new ExtendedDictionary<int, int, int>();
+            extDict[1, 2] = 10;
+            extDict[4, 5] = 20;
+            Assert.AreEqual(10, extDict[1, 2]);
+            Assert.AreEqual(20, extDict[4, 5]);
+        }
+
+        [TestMethod]
+        public void TestFastAccessByIdExpected_30_40()
+        {
+            var extDict = new ExtendedDictionary<string, int, int>();
+            extDict.Add("test", 1, 30);
+            extDict.Add("test", 4, 40);
+            extDict.Add("no_test", 6, 7);
+
+            CollectionAssert.AreEqual(new int[] { 30, 40 }, extDict.GetById("test"));
+        }
+
+        [TestMethod]
+        public void TestFastAccessByNameExpected_30_40()
+        {
+            var extDict = new ExtendedDictionary<int, string, int>();
+            extDict.Add(2, "test", 30);
+            extDict.Add(3, "test", 40);
+            extDict.Add(5, "no_test", 7);
+
+            CollectionAssert.AreEqual(new int[] { 30, 40 }, extDict.GetByName("test"));
+        }
+
+        [TestMethod]
+        public void TestCountExpected_5()
+        {
+            var extDict = new ExtendedDictionary<int, string, int>();
+            extDict.Add(2, "test", 2);
+            extDict.Add(3, "test", 5);
+            extDict.Add(5, "no_test", 7);
+            extDict.Add(6, "no_test", 7);
+            extDict.Add(7, "no_test", 7);
+
+            Assert.AreEqual(5, extDict.Count);
+        }
+
+        [TestMethod]
+        public void TestEnumirableExpected_5()
+        {
+            var extDict = new ExtendedDictionary<int, string, int>();
+            extDict.Add(2, "test", 2);
+            extDict.Add(3, "test", 5);
+            extDict.Add(5, "no_test", 7);
+            extDict.Add(6, "no_test", 7);
+            extDict.Add(7, "no_test", 7);
+
+            int tmp = 0;
+            foreach (var item in extDict)
+            {
+                tmp++;
+            }
+
+            Assert.AreEqual(5, tmp);
+        }
+
+        [TestMethod]
+        public void TestRemoveExpected_4()
+        {
+            var extDict = new ExtendedDictionary<int, string, int>();
+            extDict.Add(2, "test", 2);
+            extDict.Add(3, "test", 5);
+            extDict.Add(5, "no_test", 7);
+            extDict.Add(6, "no_test", 7);
+            extDict.Add(7, "no_test", 7);
+
+            extDict.Remove(7, "no_test");
+
+            Assert.AreEqual(4, extDict.Count);
+        }
+
+        [TestMethod]
+        public void TestClearExpected_0()
+        {
+            var extDict = new ExtendedDictionary<int, string, int>();
+            extDict.Add(2, "test", 2);
+            extDict.Add(3, "test", 5);
+            extDict.Add(5, "no_test", 7);
+            extDict.Add(6, "no_test", 7);
+            extDict.Add(7, "no_test", 7);
+
+            extDict.Clear();
+
+            Assert.AreEqual(0, extDict.Count);
+        }
+
+    
+
+        [TestMethod]
+        public void TestParallelModificationSafityExpected_4950()
+        {
+            var extDict = new ExtendedDictionary<int, string, int>();
+            extDict.Add(2, "test", 0);
+
+            Parallel.For(0, 100,
+                (i) =>
+                {
+                    extDict.EnterUpgradeableReadLock();
+                    extDict[2, "test"] += i;
+                    extDict.ExitUpgradeableReadLock();
+                });
+
+            Assert.AreEqual(4950, extDict[2, "test"]);
+        }
+    }
+}
---
+++ myCollection/ExtendedDictionary.cs
@@ -0,0 +1,403 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+
+
+namespace myCollection
+{
+
+
+
+    //Структура составного ключа.
+    public struct KeysPair<TId, TName>
+    {
+        public TId id;  
+        public TName name;
+        public KeysPair(TId id, TName name) : this()
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public override bool Equals(object ob) 
+        {
+            if (ob is KeysPair<TId, TName>)
+            {
+                KeysPair<TId, TName> c = (KeysPair<TId, TName>)ob;
+                return id.Equals(c.id) && name.Equals(c.name);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()                  
+        {
+            return id.GetHashCode() ^ name.GetHashCode();                                                    
+        }
+
+      
+
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TId"></typeparam>
+    /// <typeparam name="TName"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class ExtendedDictionary<TId, TName, TValue> : IDictionary<KeysPair<TId, TName>, TValue>
+    {
+ 
+
+        private Dictionary<KeysPair<TId, TName>, TValue> fullDict;
+
+        private Dictionary<TId, List<TValue>> fastAccesByIdDict;
+
+        private Dictionary<TName, List<TValue>> fastAccesByNameDict;
+
+        private ReaderWriterLockSlim rw_locker;
+
+        public ExtendedDictionary(int size = 0)
+        {
+            size = size >= 0 ? size : 0;
+            fullDict = new Dictionary<KeysPair<TId, TName>, TValue>(size);
+            fastAccesByIdDict = new Dictionary<TId, List<TValue>>(size);
+            fastAccesByNameDict = new Dictionary<TName, List<TValue>>(size);
+
+            rw_locker = new ReaderWriterLockSlim();
+        }
+
+        #region 
+
+        public TValue[] GetByName(TName name)  
+        {
+            try
+            {
+                rw_locker.EnterReadLock();
+                return fastAccesByNameDict[name].ToArray();         
+            }
+            finally
+            {
+                rw_locker.ExitReadLock();
+            }
+        }
+
+        public TValue[] GetById(TId id)       
+        {
+            try
+            {
+                rw_locker.EnterReadLock();
+                return fastAccesByIdDict[id].ToArray();
+            }
+            finally
+            {
+                rw_locker.ExitReadLock();
+            }
+        }
+
+        //Основной метод модификации коллекции
+        public void Add(KeysPair<TId, TName> key, TValue value) 
+        {
+            try
+            {
+                rw_locker.EnterWriteLock();
+
+                fullDict[key] = value;                          //Добавляем запись в основной словарь
+
+                if (fastAccesByIdDict.ContainsKey(key.id))
+                    fastAccesByIdDict[key.id].Add(value);    //Добавляем запись в быстрый кэш по id
+                else
+                    fastAccesByIdDict[key.id] = new List<TValue>() { value };
+
+                if (fastAccesByNameDict.ContainsKey(key.name))
+                    fastAccesByNameDict[key.name].Add(value);  //Добавляем запись в быстрый кэш по Name
+                else
+                    fastAccesByNameDict[key.name] = new List<TValue>() { value };
+            }
+            finally
+            {
+                rw_locker.ExitWriteLock();
+            }
+        }
+
+        public bool Remove(KeysPair<TId, TName> key)
+        {
+            try
+            {
+                rw_locker.EnterUpgradeableReadLock();      
+
+                TId id = key.id;
+                TName name = key.name;
+
+                if (fullDict.ContainsKey(new KeysPair<TId, TName>(id, name)))   //Если такой ключ есть
+                {
+                    try
+                    {
+                        rw_locker.EnterWriteLock();                             //Захватываем лок на модификацию.
+
+                        var tmp_key = new KeysPair<TId, TName>(id, name);
+
+                        if (fastAccesByIdDict[id].Count > 1)                 //Убираем элемент из быстрого доступа по id
+                            fastAccesByIdDict[id].Remove(fullDict[tmp_key]);
+                        else fastAccesByIdDict.Remove(id);
+
+                        if (fastAccesByNameDict[name].Count > 1)             //Убираем его из быстрого доступа по name
+                            fastAccesByNameDict[name].Remove(fullDict[tmp_key]);
+                        else fastAccesByNameDict.Remove(name);
+
+                        fullDict.Remove(tmp_key);                           //Убираем его окончательно из основного словаря.
+
+                        return true;
+                    }
+                    finally
+                    {
+                        rw_locker.ExitWriteLock();                  //Снимаем лок модификации
+                    }
+                }
+                return false;
+            }
+            finally
+            {
+                rw_locker.ExitUpgradeableReadLock();                //Снимаем лок чтения
+            }
+        }
+
+        public TValue this[TId id, TName name]
+        {
+            set
+            {
+                this.Add(id, name, value);
+            }
+            get
+            {
+                try
+                {
+                    rw_locker.EnterReadLock();
+                    return fullDict[new KeysPair<TId, TName>(id, name)];
+                }
+                finally
+                {
+                    rw_locker.ExitReadLock();
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            try
+            {
+                rw_locker.EnterWriteLock();
+                fullDict.Clear();
+                fastAccesByIdDict.Clear();
+                fastAccesByNameDict.Clear();
+            }
+            finally
+            {
+                rw_locker.ExitWriteLock();
+            }
+        }
+        #endregion
+
+        #region 
+        //Методы, которые ничего не делают, кроме как вызывают один из основных методов.
+        public void Add(TId id, TName name, TValue value)
+        {
+            this.Add(new KeysPair<TId, TName>(id, name), value);
+        }
+
+        public void Add(KeyValuePair<KeysPair<TId, TName>, TValue> item)
+        {
+            this.Add(new KeysPair<TId, TName>(item.Key.id, item.Key.name), item.Value);
+        }
+
+        public TValue this[KeysPair<TId, TName> key]
+        {
+            get
+            {
+                try
+                {
+                    rw_locker.EnterReadLock();
+                    return this[key.id, key.name];
+                }
+                finally
+                {
+                    rw_locker.ExitReadLock();
+                }
+            }
+            set
+            {
+                this.Add(key, value);
+            }
+        }
+
+        public bool Remove(TId id, TName name)
+        {
+            return this.Remove(new KeysPair<TId, TName>(id, name));
+        }
+
+        public void CopyTo(KeyValuePair<KeysPair<TId, TName>, TValue>[] array, int arrayIndex)
+        {
+           
+            try
+            {
+                rw_locker.EnterReadLock();
+                int index = arrayIndex;
+                foreach (var item in fullDict)
+                {
+                    array[index] = item;
+                    index++;
+                }
+            }
+            finally
+            {
+                rw_locker.ExitReadLock();
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }   
+        }
+        #endregion
+
+        #region
+   
+
+        public void EnterUpgradeableReadLock()        
+        {                                             
+                                                      
+            rw_locker.EnterUpgradeableReadLock();
+        }
+        public void ExitUpgradeableReadLock()
+        {
+            rw_locker.ExitUpgradeableReadLock();
+        }
+        public int Count
+        {
+            get
+            {
+                try
+                {
+                    rw_locker.EnterReadLock();
+                    return fullDict.Count();
+                }
+                finally
+                {
+                    rw_locker.ExitReadLock();
+                }
+            }
+        }
+
+       
+        public IEnumerator<KeyValuePair<KeysPair<TId, TName>, TValue>> GetEnumerator()
+        {
+            try
+            {
+                rw_locker.EnterReadLock();
+                return fullDict.GetEnumerator();
+            }
+            finally
+            {
+                rw_locker.ExitReadLock();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            try
+            {
+                rw_locker.EnterReadLock();
+                return fullDict.GetEnumerator();
+            }
+            finally
+            {
+                rw_locker.ExitReadLock();
+            }
+        }
+
+        public bool ContainsKey(KeysPair<TId, TName> key)
+        {
+            try
+            {
+                rw_locker.EnterReadLock();
+                return fullDict.ContainsKey(key);
+            }
+            finally
+            {
+                rw_locker.ExitReadLock();
+            }
+        }
+
+        public ICollection<KeysPair<TId, TName>> Keys
+        {
+            get
+            {
+                try
+                {
+                    rw_locker.EnterReadLock();
+                    return fullDict.Keys;
+                }
+                finally
+                {
+                    rw_locker.ExitReadLock();
+                }
+            }
+        }
+
+        public bool TryGetValue(KeysPair<TId, TName> key, out TValue value)
+        {
+            try
+            {
+                rw_locker.EnterReadLock();
+                return fullDict.TryGetValue(key, out value);
+            }
+            finally
+            {
+                rw_locker.ExitReadLock();
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                try
+                {
+                    rw_locker.EnterReadLock();
+                    return fullDict.Values;
+                }
+                finally
+                {
+                    rw_locker.ExitReadLock();
+                }
+            }
+        }
+
+        public bool Contains(KeyValuePair<KeysPair<TId, TName>, TValue> item)
+        {
+            try
+            {
+                rw_locker.EnterReadLock();
+                return fullDict.Contains(item);
+            }
+            finally
+            {
+                rw_locker.ExitReadLock();
+            }
+        }
+
+        public bool Remove(KeyValuePair<KeysPair<TId, TName>, TValue> item)
+        {
+            return this.Remove(item.Key);
+        }
+        #endregion
+    }
+}
+
+   
